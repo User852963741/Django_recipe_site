f4750a5bd55e0906e5179c2c7490b05cb412b938
Audrius Vegys <audrius.vegys@gmail.com>
2022-06-16 16:35:57 +0300

    working on mmodels, in progress

--- recipes/recipe_site/models.py
+++ recipes/recipe_site/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(_('name'), max_length=200, help_text=_('enter ingredient name (in plural form if possible)'))
+
+    UNITS_OF_MEASURE = (
+        ('kg', _('kilos')),
+        ('g', _('grams')),
+        ('u', _('units')),
+        ('ts', _('teaspoons')),
+        ('tbls', _('table spoons')),
+    )
+
+    measurement_unit = models.CharField(_('measured in'), max_length=1, choices=UNITS_OF_MEASURE, default='g',)
+
+    def __str__(self):
+        return f'{self.name} measured in {self.measurement_unit}'
+
+class Recipe(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='recipes',
+        verbose_name=_('owner'),
+    )
+    
